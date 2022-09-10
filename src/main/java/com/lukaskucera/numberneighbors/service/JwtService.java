626b825f988037f96303b7e6e7b871aa07ee8e2e
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-09-10 20:10:21 +0200

    GCP Secret Manager (#4)

--- src/main/java/com/lukaskucera/numberneighbors/service/JwtService.java
+++ src/main/java/com/lukaskucera/numberneighbors/service/JwtService.java
@@ -4,7 +4,6 @@ import com.lukaskucera.numberneighbors.config.JwtConfig;
 import com.nimbusds.jose.JWSAlgorithm;
 import java.time.Instant;
 import java.util.Collections;
-import java.util.Date;
 import java.util.Map;
 import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
@@ -14,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-  private static final JWSAlgorithm JWS_ALGORITHM = JWSAlgorithm.HS256;
+  private static final JWSAlgorithm JWS_ALGORITHM = JWSAlgorithm.RS256;
   private static final String TYPE_HEADER = "JWT";
   private final JwtEncoder jwtEncoder;
 
@@ -27,19 +26,20 @@ public class JwtService {
   }
 
   private String generate(String subject, Map<String, Object> claims) {
-    Instant instant = new Date().toInstant();
+    final Instant instantNow = Instant.now();
 
-    JwtClaimsSet.Builder claimsSetBuilder =
+    final JwtClaimsSet.Builder claimsSetBuilder =
         JwtClaimsSet.builder()
             .subject(subject)
             .issuer(JwtConfig.ISSUER)
             .audience(Collections.singletonList(JwtConfig.AUDIENCE))
-            .notBefore(instant)
-            .issuedAt(instant);
+            .notBefore(instantNow)
+            .issuedAt(instantNow);
 
     claims.forEach(claimsSetBuilder::claim);
 
-    JwsHeader jwsHeader = JwsHeader.with(JWS_ALGORITHM::getName).type(TYPE_HEADER).build();
+    final JwsHeader jwsHeader = JwsHeader.with(JWS_ALGORITHM::getName).type(TYPE_HEADER).build();
+
     return jwtEncoder
         .encode(JwtEncoderParameters.from(jwsHeader, claimsSetBuilder.build()))
         .getTokenValue();

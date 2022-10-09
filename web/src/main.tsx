9e321de6388727a13a494ec5f8a63b7b3d0a8f1c
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-10-09 11:11:07 +0200

    set up dependencies
    Signed-off-by: Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import App from "./app";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(
+  document.querySelector("#root") as HTMLElement,
 );
+
+root.render(<App />);

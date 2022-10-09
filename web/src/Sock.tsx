a4bc853a6d471f3b44744ddf45279ce0ae3078b0
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-10-09 10:05:43 +0200

    remove testing jwt token from code

--- web/src/Sock.tsx
+++ web/src/Sock.tsx
@@ -3,8 +3,7 @@ import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client/dist/sockjs";
 
 const Sock = (): JSX.Element => {
-  const auth =
-    "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJnYW1lSWQiOjE2LCJzdWIiOiIyMCIsImF1ZCI6Im51bWJlcm5laWdoYm9ycyIsIm5iZiI6MTY2NTMwMjA0MywiaXNzIjoiaHR0cHM6XC9cL251bWJlcm5laWdoYm9ycy5sdWthc2t1Y2VyYS5jb20iLCJuYW1lIjoiNDY1NDY1IiwiaWF0IjoxNjY1MzAyMDQzLCJhdXRob3JpdGllcyI6InBsYXllciIsInBsYXllcklkIjoyMH0.gNURp98pcKSET74Xa-Pb8ZdqPTSLthP3um1KC0FSUo3Ye0ld_vkOxkaUKa-smXsWvs-NDqV-Ewf_4XSq8-4fzQeALEgpY3xwF7frzr6z15Rng-xbXJgq8UBfYE9KFbmUPCPERnERlX_6mN0jjhzYoZYszIGylRnUmH9TKE-49rhGouUF8I85s3WGDzFoRsNBkDnkR9Uu53cUvp6O_dkjs5e7jfSRcFC51ji6UhAs2O-jkdXH8g-Zl2Ejnjm4i72VoyClG5pLIHFsqNqfVr1P-O50elVw272ONVlOTUxBI7hbhGD9qHMHhR_HR4v2_-0nMeUdfoXxDqvluD3I2fNpQw";
+  const auth = "";
 
   const stompClient = new Client({
     connectHeaders: {

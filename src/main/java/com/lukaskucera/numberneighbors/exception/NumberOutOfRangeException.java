86ba2c1e7ae77333c502104620ce9ad3a66dfa10
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-05 17:48:26 +0100

    keep String.format in NumberOutOfRangeException for readability
    Signed-off-by: Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>

--- src/main/java/com/lukaskucera/numberneighbors/exception/NumberOutOfRangeException.java
+++ src/main/java/com/lukaskucera/numberneighbors/exception/NumberOutOfRangeException.java
@@ -6,13 +6,12 @@ public class NumberOutOfRangeException extends RuntimeException {
 
   public NumberOutOfRangeException(int number) {
     super(
-      "number " +
-      number +
-      " is not in the range [" +
-      NumberServiceImpl.MIN_NUMBER +
-      ", " +
-      NumberServiceImpl.MAX_NUMBER +
-      "]"
+      String.format(
+        "Number %d is not in the range [%d, %d]",
+        number,
+        NumberServiceImpl.MIN_NUMBER,
+        NumberServiceImpl.MAX_NUMBER
+      )
     );
   }
 }

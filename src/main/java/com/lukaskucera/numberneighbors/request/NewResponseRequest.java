3a66b88ece75083792ad1ae1b8a7819fddef5c04
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-05 12:35:18 +0100

    remove validation package
    Signed-off-by: Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>

--- src/main/java/com/lukaskucera/numberneighbors/request/NewResponseRequest.java
+++ src/main/java/com/lukaskucera/numberneighbors/request/NewResponseRequest.java
@@ -1,9 +1,6 @@
 package com.lukaskucera.numberneighbors.request;
 
 import com.lukaskucera.numberneighbors.enums.ResponseType;
-import com.lukaskucera.numberneighbors.validation.EnumValidator;
 import javax.validation.constraints.NotNull;
 
-public record NewResponseRequest(
-  @NotNull @EnumValidator(ResponseType.class) ResponseType type
-) {}
+public record NewResponseRequest(@NotNull ResponseType type) {}

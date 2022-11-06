ed68a4044f558e5bc645c729aadd7efffa4bbf68
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-06 15:11:17 +0100

    add NativeHint with aotProxies
    Signed-off-by: Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>

--- src/main/java/com/lukaskucera/numberneighbors/NumberNeighborsApplication.java
+++ src/main/java/com/lukaskucera/numberneighbors/NumberNeighborsApplication.java
@@ -4,12 +4,45 @@ import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ProxyBits;
 import org.springframework.nativex.hint.TypeHint;
 
 @SpringBootApplication
-@TypeHint(
-  types = PostgreSQL10Dialect.class,
-  typeNames = "org.hibernate.dialect.PostgreSQLDialect"
+@NativeHint(
+  types = {
+    @TypeHint(
+      types = PostgreSQL10Dialect.class,
+      typeNames = "org.hibernate.dialect.PostgreSQLDialect"
+    ),
+  },
+  aotProxies = {
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.AnswerServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.GameServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.NumberServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.PlayerServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.ResponseServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+    @AotProxyHint(
+      targetClass = com.lukaskucera.numberneighbors.service.TurnServiceImpl.class,
+      proxyFeatures = ProxyBits.IS_STATIC
+    ),
+  }
 )
 @EnableCaching(proxyTargetClass = true)
 public class NumberNeighborsApplication {

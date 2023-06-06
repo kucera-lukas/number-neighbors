a759ec57aff32a510755fd71ac65fb316ffaa56b
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-06-06 20:17:37 +0200

    use stomp client on each page (#194)

--- web/src/pages/game/play.tsx
+++ web/src/pages/game/play.tsx
@@ -1,48 +1,11 @@
 import OpponentNumbers from "../../components/play/opponent-numbers.component";
 import UserNumbers from "../../components/play/user-numbers.component";
 import { useGamePayload } from "../../context/game-payload.context";
-import { useTurns } from "../../context/turns.context";
-import useLocalStorageItem from "../../hooks/localstorage.hook";
-import useStompClient from "../../hooks/stomp-client.hook";
 import PageLayout from "../../layouts/page.layout";
 
-import type GamePayload from "../../types/game-payload.type";
-import type TurnPayload from "../../types/turn-payload.type";
-import type { IMessage } from "@stomp/stompjs";
-
-const STOMP_ENDPOINT = "play";
-const PAYLOAD_DESTINATION = "/user/queue/payload";
-const TURNS_DESTINATION = "/user/queue/turns";
-
 const Play = (): JSX.Element => {
-  const [gamePayload, setGamePayload] = useGamePayload();
-  const [, setTurns] = useTurns();
-  const [token] = useLocalStorageItem<string>("token");
+  const [gamePayload] = useGamePayload();
   const enabled = gamePayload?.ready;
-
-  const onPayload = (message: IMessage) =>
-    setGamePayload(JSON.parse(message.body) as GamePayload);
-
-  const onTurn = (message: IMessage) => {
-    const turn = JSON.parse(message.body) as TurnPayload;
-
-    setTurns((turns) => {
-      if (turns === undefined) {
-        turns = [turn];
-      } else if (turn.id === turns.at(-1)?.id) {
-        turns[turns.length - 1] = turn;
-      } else {
-        turns.push(turn);
-      }
-
-      return turns;
-    });
-  };
-
-  useStompClient(STOMP_ENDPOINT, token, (client) => {
-    client.subscribe(PAYLOAD_DESTINATION, onPayload);
-    client.subscribe(TURNS_DESTINATION, onTurn);
-  });
 
   return (
     <PageLayout title="play">

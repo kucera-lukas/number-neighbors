6534dc94481ac349451839af30eb72fd68d1df38
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-06-08 13:46:55 +0200

    fetch turns on game load

--- web/src/layouts/page.layout.tsx
+++ web/src/layouts/page.layout.tsx
@@ -1,5 +1,6 @@
 import { SERVER_URI } from "../config/environment";
 import { useGamePayload } from "../context/game-payload.context";
+import { useTurns } from "../context/turns.context";
 import useLocalStorageItem from "../hooks/localstorage.hook";
 import useStompClient from "../hooks/stomp-client.hook";
 
@@ -8,6 +9,7 @@ import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import type GamePayload from "../types/game-payload.type";
+import type TurnPayload from "../types/turn-payload.type";
 import type { PropsWithChildren } from "react";
 
 const BASE_TITLE = "number-neighbors";
@@ -20,6 +22,7 @@ const PageLayout = ({ children, title }: PageLayoutProps): JSX.Element => {
   const navigate = useNavigate();
   const params = useParams();
   const [, setGamePayload] = useGamePayload();
+  const [, setTurns] = useTurns();
   const gameId = params.gameId;
   const [token] = useLocalStorageItem<string>("token");
 
@@ -50,6 +53,28 @@ const PageLayout = ({ children, title }: PageLayoutProps): JSX.Element => {
         setGamePayload(res);
       });
   }, [gameId, token, setGamePayload]);
+
+  useEffect(() => {
+    if (!gameId || !token) {
+      return;
+    }
+
+    void fetch(`${SERVER_URI}/turns?game=${gameId}`, {
+      method: "GET",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res: TurnPayload[]) => {
+        setTurns(res);
+      });
+  }, [gameId, token, setTurns]);
 
   return (
     <Center>

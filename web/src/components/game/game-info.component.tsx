0227ee608f7a15de0932af9038424f18c6a24e6b
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-06-09 20:31:08 +0200

    replace user ids with names in the game info page

--- web/src/components/game/game-info.component.tsx
+++ web/src/components/game/game-info.component.tsx
@@ -1,7 +1,9 @@
+import GameInfoBadge from "./game-info-badge";
+
 import { useGamePayload } from "../../context/game-payload.context";
 import AccordionItemLayout from "../../layouts/accordion-item.layout";
 
-import { List, Stack, Text } from "@mantine/core";
+import { Stack, Text } from "@mantine/core";
 
 const GameInfo = (): JSX.Element => {
   const [gamePayload] = useGamePayload();
@@ -11,24 +13,20 @@ const GameInfo = (): JSX.Element => {
       title="Game Info"
       value="game-info"
     >
-      <Stack spacing="xs">
-        <List>
-          <List.Item>
-            <Text size="sm">
-              <>Game ID: {gamePayload?.id}</>
-            </Text>
-          </List.Item>
-          <List.Item>
-            <Text size="sm">
-              <>Player ID: {gamePayload?.user?.id}</>
-            </Text>
-          </List.Item>
-          <List.Item>
-            <Text size="sm">
-              <>Opponent player ID: {gamePayload?.opponent?.id}</>
-            </Text>
-          </List.Item>
-        </List>
+      <Stack
+        spacing="xs"
+        align="center"
+        justify="center"
+      >
+        <GameInfoBadge
+          player={gamePayload?.user}
+          color="blue"
+        />
+        <Text size="sm">vs</Text>
+        <GameInfoBadge
+          player={gamePayload?.opponent}
+          color="red"
+        />
       </Stack>
     </AccordionItemLayout>
   );

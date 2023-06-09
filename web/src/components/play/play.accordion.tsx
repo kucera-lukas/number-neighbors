431aa6306b05ee1fd55bb9a764930cc73e49158e
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2023-06-09 20:59:53 +0200

    maintain the internal accordion state on the play page (#216)

--- web/src/components/play/play.accordion.tsx
+++ web/src/components/play/play.accordion.tsx
@@ -3,26 +3,19 @@ import OpponentNumbers from "./opponent-numbers.component";
 import TurnHistory from "./turn-history.component";
 import UserNumbers from "./user-numbers.component";
 
-import { useTurns } from "../../context/turns.context";
 import AccordionLayout from "../../layouts/accordion.layout";
 
-import { memo, useState } from "react";
+import { memo } from "react";
 
 // we want to maintain the internal accordion state in each re-render
 const PlayAccordion = memo(() => {
-  const [turns] = useTurns();
-  const [turnIndex, setTurnIndex] = useState<number>(turns.length - 1);
-
   return (
     <AccordionLayout
       defaultValues={["user-numbers", "opponent-numbers", "current-turn"]}
     >
       <UserNumbers />
       <OpponentNumbers />
-      <CurrentTurn
-        turnIndex={turnIndex}
-        setTurnIndex={setTurnIndex}
-      />
+      <CurrentTurn />
       <TurnHistory />
     </AccordionLayout>
   );
